dbcd8027c9da8cb8005d019c26561f30068f9df5
Edward Druscki <edward.druscki@gmail.com>
2021-05-31 16:55:15 +0300

    Epel release added

--- provision.sh
+++ provision.sh
@@ -4,7 +4,7 @@ echo "Provision Load Balanced LB1"
 sudo yum clean all
 sudo yum update -y
 sudo yum install -y yum-utils
-sudo yum install epel-release
-sudo yum install nginx
+sudo yum install epel-release -y 
+sudo yum install nginx -y 
 sudo systemctl enable nginx
 sudo systemctl start nginx
