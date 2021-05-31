8e7762aa45c4b7f98e84280a305152b0651ad4a5
edrus06 <edward.druscki@gmail.com>
2021-05-31 21:00:00 +0300

    Create provision-host.sh

---
+++ provision-host.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Provision Load Balanced LB1"
+sudo yum clean all
+sudo yum update -y
+sudo yum install -y yum-utils
+sudo yum install epel-release -y 
+sudo yum install nginx -y 
+
+sudo systemctl start nginx.service
+sudo systemctl enable nginx.service
+
+echo "Install necessary packages"
+sudo yum install -y vim net-tools git
+
+echo "Create backup of nginx.conf"
+sudo systemctl stop nginx.service
+
+sudo cp /etc/nginx/nginx.conf /tmp/nginx-conf-$(date +"%m-%d-%y-%T")
